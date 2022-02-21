3fe96afb42ade9f63af3d3789ad69719032058f2
mattdepillis <mattdepillis@gmail.com>
2022-02-21 18:10:00 -0500

    solved the autocorrect strings problem from Coder's apprentice, extra difficulty with period handling

--- python-basics/strings.py
+++ python-basics/strings.py
@@ -90,5 +90,62 @@ def reorder_by_ascii(string):
 ###################################################################################################################################
 """
   5. Write an autocorrect function:
-  -- 
+  * second capital letter of a word made lowercase.
+  * duplicate word removed. DONE
+  * word starting sentence should be capitalized. DONE
+  * if word contains all capitals after first letter, capitalization should be reversed.
+  * days of the week should be capitalized. DONE
+  * handle ellipsis and periods properly. DONE
 """
+sentence = """as it turned out our chance meeting with REverend
+aRTHUR BElling was was to change our whole way of life, and
+every sunday we'd hurry along to to St lOONY up the Cream BUn
+and Jam...this was the time of our lives. Here's what I think."""
+
+def autocorrect(string):
+  autocorrected_string = ""
+  days_of_week = [
+    "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
+  ]
+
+  # split the string by sentence-ending periods -- not handling other punctuation.
+  split_string = re.split('[.]', string.strip().replace('\n', ' '))
+  for clause in split_string:
+    # if the clause is empty, it was a period. add it to autocorrected string + continue.
+    if (len(clause) == 0):
+      autocorrected_string += clause.join('.')
+      continue
+
+    revised_string = ""
+    split_clause = re.split('\s', clause)
+  
+    for i in range(len(split_clause)):
+      # if first word in clause:
+      if (i == 0):
+        if (len(autocorrected_string) > 0):
+          autocorrected_string += "."
+          # if the current str len > 3 and doesn't have ellipsis at end, don't capitalize
+          if ((len(autocorrected_string) > 3 and autocorrected_string[-3:] == "...")):
+            # string should already contain leading space; no need to prepend.
+            revised_string += split_clause[i]
+        else:
+          revised_string += split_clause[i].capitalize()
+      else:
+        if (len(revised_string) > len(split_clause[i])):
+          if (revised_string[-len(split_clause[i]):].strip() == split_clause[i]):
+            continue
+        
+        revised_string += " "
+
+        if (re.match("^[A-Z][A-Z]", split_clause[i])):
+          revised_string += split_clause[i].capitalize()
+        elif (re.match("[A-Z]+", split_clause[i][1:])):
+          revised_string += split_clause[i].lower().capitalize()
+        elif (split_clause[i].lower() in days_of_week):
+          revised_string += split_clause[i].capitalize()
+        else:
+          revised_string += split_clause[i]
+
+    autocorrected_string += revised_string
+        
+  return autocorrected_string
