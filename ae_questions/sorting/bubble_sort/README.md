36e11d34f8606af426dbca893cb81d3158ae6f7e
mattdepillis <mattdepillis@gmail.com>
2022-07-12 17:53:38 -0400

    bubble sort ae solution; readme setup; sorting folder setup

---
+++ ae_questions/sorting/bubble_sort/README.md
@@ -0,0 +1,22 @@
+---
+Problem: Bubble Sort
+Type: Sort
+Date Written: 07-12-2022
+---
+
+# Problem
+Write a function that takes an array of integers and returns that array sorted using the bubble sort algorithm.
+
+For example, python```bubble_sort([2, 3, 1, 5, 6, 4])``` should return ```[1, 2, 3, 4, 5, 6]```.
+
+# Solution
+Solution explanation here...
+
+**Pseudocode:**
+- psuedocode here...
+
+**Big O:**
+- O(n^2) time
+  - 
+- O(1) space
+  - 
---
+++ ae_questions/sorting/bubble_sort/solution.py
@@ -0,0 +1,19 @@
+"""
+Write a function that takes an array of integers and returns that array sorted using the bubble sort algorithm.
+"""
+def bubble_sort(array):
+  iterations = 0
+  swapped = True
+
+  while(swapped):
+    swapped = False
+    for i in range(len(array) - iterations - 1):
+      if array[i] > array[i + 1]:
+        swapped = True
+        array[i], array[i + 1] = array[i + 1], array[i]
+  
+  return array
+
+ 
+if __name__ == "__main__":
+  print(bubble_sort([1, 8, 2, 6, 4, 5, 10, 7]))
