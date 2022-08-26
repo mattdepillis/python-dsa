387b3d79d99a75beb506fcc3937b45a393e086e1
mattdepillis <mattdepillis@gmail.com>
2022-08-26 17:00:24 -0400

    inorder and postorder traversal methods

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -30,27 +30,63 @@ class Tree:
         self.insert_node(current.right, value)
     else:
       return
+
+  """
+  inorder traversal method.
+    traverses left -> root -> right.
+  """
+  def inorder_traversal(self, node, list):
+    if node.left:
+      self.inorder_traversal(node.left, list)
+    if node:
+      list.append(node.value)
+    if node.right:
+      self.inorder_traversal(node.right, list)
+    return list
     
   """
   preorder traversal method.
-    starts with root node, traverses all the way left, then makes its way right across the tree.
+    traverses root -> left -> right.
   """
   def preorder_traversal(self, node, list):
     if node:
       list.append(node.value)
-      if node.left:
-        self.preorder_traversal(node.left, list)
-      if node.right:
-        self.preorder_traversal(node.right, list)
+    if node.left:
+      self.preorder_traversal(node.left, list)
+    if node.right:
+      self.preorder_traversal(node.right, list)
+
+    return list
+
+  """
+  postorder traversal method.
+    traverses the tree left -> right -> root.
+  """
+  def postorder_traversal(self, node, list):
+    if node.left:
+      self.postorder_traversal(node.left, list)
+    if node.right:
+      self.postorder_traversal(node.right, list)
+    if node:
+      list.append(node.value)
 
     return list
 
 
 if __name__ == "__main__":
   # 3 is a low number; this will be a very lopsided tree (most nodes on the right).
-  tree = Tree(3)
-  nodes = [1, 5, 2, 4, 6, 8, 7, 12, 9, 10, 11]
+  # tree = Tree(3)
+  # nodes = [1, 5, 2, 4, 6, 8, 7, 12, 9, 10, 11]
+
+  # tree = Tree(10)
+  # nodes = [5, 11, 13, 4, 7, 6, 3, 12, 8, 2, 1, 15, 14, 20]
+
+  tree = Tree(13)
+  nodes = [5, 4, 3, 6, 7, 23, 22, 28, 32, 27]
+
   for node in nodes:
     tree.insert_node(tree.root, node)
   
-  print(tree.preorder_traversal(tree.root, list=[]))
+  # print(tree.preorder_traversal(tree.root, list=[]))
+  # print(tree.inorder_traversal(tree.root, list=[]))
+  print(tree.postorder_traversal(tree.root, list=[]))
