4ae73b6d2c7be2d6560b616c960c308874097c55
mattdepillis <mattdepillis@gmail.com>
2022-09-23 10:05:32 -0400

    comments on get_permutations

--- ae_questions/recursion/medium/permutations/solution.py
+++ ae_questions/recursion/medium/permutations/solution.py
@@ -3,12 +3,17 @@ Given an array of integers and a target integer, return the array such that all 
 TC: O(n*n!), SC: O(n*n!)
 """
 def get_permutations(array, arr, r):
+  # return condition is: array is empty + temp array has length
+  # guarantees all elements in the temp array
   if not len(array) and len(arr):
     r.append(arr)
   else:
     for item in array:
       new_array = [x for x in array if x != item]
       new_arr = arr + [item]
+      # calling r.append(get_permutations(...)) here will cause circular reference,
+      # appending r itself to r (resulting in [...] inclusion in the final returned value).
+      # instead, simply recurse a level down and return once return condition hits.
       get_permutations(new_array, new_arr, r)
 
   return r
