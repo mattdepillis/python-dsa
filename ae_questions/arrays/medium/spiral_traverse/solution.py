53f158ff17c33bc9da7d1b921997733ab7ed7674
mattdepillis <mattdepillis@gmail.com>
2022-11-08 10:05:30 -0500

    solved spiral_traverse -- solution1

--- ae_questions/arrays/medium/spiral_traverse/solution.py
+++ ae_questions/arrays/medium/spiral_traverse/solution.py
@@ -11,17 +11,29 @@ def spiral_traverse(matrix):
     w = h = iterations
 
     for i in range(iterations, width):
-      spiral.append(matrix[h][i])
+      if matrix[h][i]:
+        spiral.append(matrix[h][i])
+        matrix[h][i] = False
       w = i
     for j in range(iterations + 1, height):
-      spiral.append(matrix[j][w])
+      print(matrix[j][w])
+      if matrix[j][w]:
+        spiral.append(matrix[j][w])
+        matrix[j][w] = False
       h = j
     for i in range(width - 2, iterations - 1, -1):
-      spiral.append(matrix[h][i])
+      if matrix[h][i]:
+        spiral.append(matrix[h][i])
+        matrix[h][i] = False
       w = i
     for j in range(height - 2, iterations, -1):
-      spiral.append(matrix[j][w])
+      if matrix[j][w]:
+        spiral.append(matrix[j][w])
+        matrix[j][w] = False
       h = j
+
+    if len(spiral) == len(matrix) * len(matrix[0]):
+      break
   
     width, height, iterations = width - 1, height - 1, iterations + 1
 
@@ -29,9 +41,22 @@ def spiral_traverse(matrix):
 
 
 if __name__ == "__main__":
+  # print(spiral_traverse([
+  #   [1, 2, 3, 4],
+  #   [12, 13, 14, 5],
+  #   [11, 16, 15, 6],
+  #   [10, 9, 8, 7]
+  # ]))
+
+  # print(spiral_traverse([
+  #   [1],
+  #   [2],
+  #   [3],
+  #   [4],
+  #   [5],
+  #   [6]
+  # ]))
+
   print(spiral_traverse([
-    [1, 2, 3, 4],
-    [12, 13, 14, 5],
-    [11, 16, 15, 6],
-    [10, 9, 8, 7]
+    [1, 2, 3, 4, 5, 6, 7]
   ]))
