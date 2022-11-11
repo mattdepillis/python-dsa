81a6543ebbd01805e566ea537175bba42886c78d
mattdepillis <mattdepillis@gmail.com>
2022-11-11 16:38:22 -0500

    first solution to selection sort - misunderstanding = non-optimal SC

--- ae_questions/sorting/easy/selection_sort/solution.py
+++ ae_questions/sorting/easy/selection_sort/solution.py
@@ -1,11 +1,22 @@
 """
 Given an array of integers, sort it using the selection sort algorithm.
 
-TC: O()
-SC: O()
+TC: O(n^2) -- we need to iterate through each index n times -- leading to a n^2 TC
+SC: O(n) -- storing values in new array. We can actually do better than this - I thought I was shooting for optimal SC = O(n).
 """
 def selection_sort(array):
-  return array
+  sorted = []
+  while len(array):
+    min_value, min_index = array[0], 0
+
+    for i in range(1, len(array)):
+      if array[i] < min_value:
+        min_value, min_index = array[i], i
+
+    sorted.append(min_value)
+    array.pop(min_index)
+
+  return sorted
 
 
 if __name__ == "__main__":
