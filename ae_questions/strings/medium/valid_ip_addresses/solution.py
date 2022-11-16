55fd9126d98f1b92652e4519ee1f1a6207fa09b1
mattdepillis <mattdepillis@gmail.com>
2022-11-15 21:19:24 -0500

    tidied up solution for valid_ip_addresses

--- ae_questions/strings/medium/valid_ip_addresses/solution.py
+++ ae_questions/strings/medium/valid_ip_addresses/solution.py
@@ -1,9 +1,14 @@
 """
 Given a string of length <= 12, write a function that returns all the possible IP addresses that can be created be inserting 3 "."s into the string. Reminder: the integers (each 1-3 digit num separated by the .) in the IP address must be from 0-255.
 """
+# determines whether a digit combo is valid.
 def is_valid(string):
+  if len(string) > 1 and string[0] == "0":
+    return False
   return int(string) < 256
 
+# loops through to find valid combinations of digits.
+# when 3 previous iterations have completed, checks to see whether the current result is valid.
 def find_valid(string, shift, iterations, list, combo):
   for i in range(shift + 1, shift + 4):
     if i > len(string): return
@@ -16,12 +21,12 @@ def find_valid(string, shift, iterations, list, combo):
           list.append('.'.join(c))
   return list
 
+# base setup function
 def valid_ip_addresses(string):
-  valid, combo = [], []
-  iterations = shift = 0
-  find_valid(string, shift, iterations, valid, combo)
-  return valid
+  return find_valid(string, shift=0, iterations=0, list=[], combo=[]) or []
 
 
 if __name__ == "__main__":
-  print(valid_ip_addresses("1921680"))
+  print(valid_ip_addresses("1921680")) # ["1.9.216.80", "1.92.16.80", "1.92.168.0", "19.2.16.80", "19.2.168.0", "19.21.6.80", "19.21.68.0", "19.216.8.0", "192.1.6.80", "192.1.68.0", "192.16.8.0"]
+
+  print(valid_ip_addresses("11")) # []
