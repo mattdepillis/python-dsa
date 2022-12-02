e908a2ac3981318e6bb41e0da3ff8eef72759f7e
mattdepillis <mattdepillis@gmail.com>
2022-12-02 12:42:45 -0500

    TC+SC for staircase_traversal

--- ae_questions/recursion/medium/staircase_traversal/solution.py
+++ ae_questions/recursion/medium/staircase_traversal/solution.py
@@ -1,5 +1,9 @@
 """
 Given two positive integers representing the height of a staircase and the max number of steps you can advance up the staircase at a time, write a function that returns the number of ways in which you can advance up the staircase.
+
+Recursive solution:
+TC: O(k^n), where k is max steps and n is the total number of stairs to climb. Since the for-loop must start at 1, you must travel max n levels for recursive depth, and each recursed level down there will be k executions of the for-loop.
+SC: O(n) -- must store n-proportional number of recursive calls on the stack at a given time
 """
 def helper(stairs_remaining, max_steps, ways):
   for i in range(1, max_steps + 1):
