fa94b65e57ef143e9aa548fe79b9f7fd58b0d918
mattdepillis <mattdepillis@gmail.com>
2022-12-06 18:07:18 -0500

    debugged initial solution for simple fix - did TC and SC

--- ae_questions/graphs/medium/youngest_common_ancestor/solution.py
+++ ae_questions/graphs/medium/youngest_common_ancestor/solution.py
@@ -1,5 +1,8 @@
 """
 Given 3 inputs, which are all instances of a node class that has name and direct ancestor k-v pairs, find the closest common ancestor to the two provided nodes to search.
+
+TC: O(d) -- where d = height of the tree-shaped graph
+SC: O(1) -- SC is constant regardless of number of nodes in the graph
 """
 class Node:
   def __init__(self, name, ancestor):
@@ -12,9 +15,9 @@ def construct_graph(nodes, graph):
   return graph
 
 def node_depth(top, node):
-  depth, current = 1, node
-  while current != top:
-    current = current.ancestor
+  depth = 1
+  while node != top:
+    node = node.ancestor
     depth += 1
   return depth
 
@@ -30,7 +33,7 @@ def get_youngest_common_ancestor(top_ancestor, descendant_one, descendant_two):
     greater_depth, smaller_depth = (d1_depth, d2_depth) if d1_depth > d2_depth else (d2_depth, d1_depth)
     while greater_depth > smaller_depth:
       if deep.ancestor == shallow:
-        return shallow.ancestor
+        return shallow
       greater_depth -= 1
       deep = deep.ancestor
 
@@ -41,17 +44,46 @@ def get_youngest_common_ancestor(top_ancestor, descendant_one, descendant_two):
     a1, a2 = a1.ancestor, a2.ancestor
 
 if __name__ == "__main__":
+  # nodes = [
+  #   {"ancestor": None, "id": "A", "name": "A"},
+  #   {"ancestor": "A", "id": "B", "name": "B"},
+  #   {"ancestor": "A", "id": "C", "name": "C"},
+  #   {"ancestor": "B", "id": "D", "name": "D"},
+  #   {"ancestor": "B", "id": "E", "name": "E"},
+  #   {"ancestor": "C", "id": "F", "name": "F"},
+  #   {"ancestor": "C", "id": "G", "name": "G"},
+  #   {"ancestor": "D", "id": "H", "name": "H"},
+  #   {"ancestor": "D", "id": "I", "name": "I"}
+  # ]
+
   nodes = [
     {"ancestor": None, "id": "A", "name": "A"},
     {"ancestor": "A", "id": "B", "name": "B"},
     {"ancestor": "A", "id": "C", "name": "C"},
-    {"ancestor": "B", "id": "D", "name": "D"},
-    {"ancestor": "B", "id": "E", "name": "E"},
-    {"ancestor": "C", "id": "F", "name": "F"},
-    {"ancestor": "C", "id": "G", "name": "G"},
-    {"ancestor": "D", "id": "H", "name": "H"},
-    {"ancestor": "D", "id": "I", "name": "I"}
+    {"ancestor": "A", "id": "D", "name": "D"},
+    {"ancestor": "A", "id": "E", "name": "E"},
+    {"ancestor": "A", "id": "F", "name": "F"},
+    {"ancestor": "B", "id": "G", "name": "G"},
+    {"ancestor": "B", "id": "H", "name": "H"},
+    {"ancestor": "B", "id": "I", "name": "I"},
+    {"ancestor": "C", "id": "J", "name": "J"},
+    {"ancestor": "D", "id": "K", "name": "K"},
+    {"ancestor": "D", "id": "L", "name": "L"},
+    {"ancestor": "F", "id": "M", "name": "M"},
+    {"ancestor": "F", "id": "N", "name": "N"},
+    {"ancestor": "H", "id": "O", "name": "O"},
+    {"ancestor": "H", "id": "P", "name": "P"},
+    {"ancestor": "H", "id": "Q", "name": "Q"},
+    {"ancestor": "H", "id": "R", "name": "R"},
+    {"ancestor": "K", "id": "S", "name": "S"},
+    {"ancestor": "P", "id": "T", "name": "T"},
+    {"ancestor": "P", "id": "U", "name": "U"},
+    {"ancestor": "R", "id": "V", "name": "V"},
+    {"ancestor": "V", "id": "W", "name": "W"},
+    {"ancestor": "V", "id": "X", "name": "X"},
+    {"ancestor": "V", "id": "Y", "name": "Y"},
+    {"ancestor": "X", "id": "Z", "name": "Z"}
   ]
-  graph = construct_graph(nodes, {})
 
-  print(get_youngest_common_ancestor(graph['A'], graph['E'], graph['I']))
+  graph = construct_graph(nodes, {})
+  print(get_youngest_common_ancestor(graph['A'], graph['T'], graph['H']).name)
