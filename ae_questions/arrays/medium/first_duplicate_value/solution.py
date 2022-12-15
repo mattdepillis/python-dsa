48e259a279ee0c101beb72593c19907467f46506
mattdepillis <mattdepillis@gmail.com>
2022-12-15 17:00:23 -0500

    initial suboptimal solutions for first_duplicate_value -- misread prompt

---
+++ ae_questions/arrays/medium/first_duplicate_value/solution.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+
+# O(n) ST solution
+def first_duplicate_value(array):
+  s = set()
+  for i in array:
+    if i in s:
+      return i
+    s.add(i)
+
+  return -1
+
+
+# O(n^2) TC, O(1) SC
+# def first_duplicate_value(array):
+#   for i in range(len(array)):
+#     if isinstance(array[i], list): continue
+#     for j in range(i + 1, len(array)):
+#       if array[j] == array[i]:
+#         array[j] = [array[j]]
+#         break
+
+#   for i in array:
+#     if isinstance(i, list): return i[0]
+#   return -1
+
+
+if __name__ == "__main__":
+  print(first_duplicate_value([2, 1, 5, 2, 3, 3, 2, 4]))
