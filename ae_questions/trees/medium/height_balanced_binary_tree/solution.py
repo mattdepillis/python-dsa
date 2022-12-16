22b33813f3f28d4ed49f21444a59f94ced41946f
mattdepillis <mattdepillis@gmail.com>
2022-12-16 12:11:14 -0500

    solved height_balanced_binary_tree

---
+++ ae_questions/trees/medium/height_balanced_binary_tree/solution.py
@@ -0,0 +1,52 @@
+"""
+
+"""
+import sys
+from os import path
+
+sys.path.append(path.dirname( path.dirname ( path.dirname ( path.abspath(__file__) ) ) ))
+
+from ae_bst import Tree as t
+
+
+def find_depths_at_node(node, balanced):
+  max_left = max_right = 0
+
+  if node.left:
+    ml, bl = find_depths_at_node(node.left, balanced)
+    if not bl: return (0, False)
+    max_left += 1 + ml
+  if node.right:
+    hr, br = find_depths_at_node(node.right, balanced)
+    if not br: return (0, False)
+    max_right += 1 + hr
+
+  if abs(max_left - max_right) > 1:
+    balanced = False
+
+  return max(max_left, max_right), balanced
+
+
+def height_balanced_binary_tree(node):
+  return find_depths_at_node(node, balanced=True)[1]
+
+
+if __name__ == "__main__":
+  nodes = [
+    {"id": "1", "left": "2", "right": "3", "value": 1},
+    {"id": "2", "left": "4", "right": "5", "value": 2},
+    {"id": "3", "left": None, "right": "6", "value": 3},
+    {"id": "4", "left": None, "right": None, "value": 4},
+    {"id": "5", "left": "7", "right": "8", "value": 5},
+    {"id": "6", "left": None, "right": None, "value": 6},
+    {"id": "7", "left": None, "right": None, "value": 7},
+    {"id": "8", "left": None, "right": None, "value": 8}
+  ]
+
+  tree = t()
+
+  for node in nodes:
+    tree.insert_node(tree.root, node)
+
+  # print(tree.preorder_traversal(tree.root, list=[]))
+  print(height_balanced_binary_tree(tree.root))
