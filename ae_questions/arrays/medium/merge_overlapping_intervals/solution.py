532c2d01bbbc7d53150b8318a1d864894771cf4f
mattdepillis <mattdepillis@gmail.com>
2022-12-21 16:37:54 -0500

    solved merge_overlapping_intervals with proper requirements

--- ae_questions/arrays/medium/merge_overlapping_intervals/solution.py
+++ ae_questions/arrays/medium/merge_overlapping_intervals/solution.py
@@ -2,26 +2,29 @@
 Given a list of intervals (provided in start, end pairs in sublists), return a new list of the intervals from that list that intersect.
 """
 def merge_overlapping_intervals(intervals):
-  overlapping, sorted = [], []
+  merged = []
 
   intervals.sort()
 
   for i in range(len(intervals)):
-    for num in intervals[i]:
-      if i > 0 and num <= sorted[-1]:
-        if intervals[i - 1] not in overlapping: overlapping.append(intervals[i - 1])
-        if overlapping[-1] != intervals[i]: overlapping.append(intervals[i])
-      sorted.append(num)
-      sorted.sort()
+    if i > 0 and intervals[i][0] <= merged[-1][1]:
+      if intervals[i][1] > merged[-1][1]: merged[-1][1] = intervals[i][1]
+      continue
+    merged.append(intervals[i])
 
-  return overlapping
+  return merged
 
 
 if __name__ == "__main__":
-  print(merge_overlapping_intervals([
-    [1, 2], [3, 5], [4, 7], [6, 8], [9, 10]
-  ]))
+  # print(merge_overlapping_intervals([
+  #   [1, 2], [3, 5], [4, 7], [6, 8], [9, 10]
+  # ]))
+
+  # print(merge_overlapping_intervals([
+  #   [89, 90], [-10, 20], [-50, 0], [70, 90], [90, 91], [90, 95]
+  # ]))
 
   print(merge_overlapping_intervals([
-    [89, 90], [-10, 20], [-50, 0], [70, 90], [90, 91], [90, 95]
+    [1, 22],
+    [-20, 30]
   ]))
