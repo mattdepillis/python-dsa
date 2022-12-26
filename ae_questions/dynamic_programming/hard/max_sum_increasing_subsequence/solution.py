648d60b95de62c2c931231410e02869fe3c3143f
mattdepillis <mattdepillis@gmail.com>
2022-12-26 13:55:41 -0500

    starting over increasing subseq with dynamic programming strategy

--- ae_questions/dynamic_programming/hard/max_sum_increasing_subsequence/solution.py
+++ ae_questions/dynamic_programming/hard/max_sum_increasing_subsequence/solution.py
@@ -1,25 +1,37 @@
 """
 Write a function that takes a list of integers and returns the greatest sum that can be generated from a subsequence of the array consisting of strictly increasing values.
 """
-def max_sum_increasing_subsequence(array):
-  subarrays, max_subsequence = [[array[0]]], [0, []]
-  
-  for i in range(1, len(array)):
-    added = False
-    for subarray in subarrays:
-      if array[i] > subarray[-1]:
-        subarray.append(array[i])
-        added = True
-    if not added:
-      new_subarray = [array[i]]
-      for j in reversed(range(0, i)):
-        if array[j] < new_subarray[0]: new_subarray.insert(0, array[j])
-      subarrays.append(new_subarray)
+# def max_sum_increasing_subsequence(array):
+#   max_subsequence = [float('-inf'), []]
 
-  for subarray in subarrays:
-    if sum(subarray) > max_subsequence[0]: max_subsequence = [sum(subarray), subarray]
+#   for i in range(len(array)):
+#     # print('*****************\n array[i]', array[i], '\n*************')
+#     subseq = [array[i]]
+#     for j in reversed(range(0, i)):
+#       # print('array[j]', array[j])
+#       if array[j] < subseq[0] and array[j] > 0:
+#         subseq.insert(0, array[j])
+#       elif array[j] > subseq[0]:
+#         if array[j] >= array[i]: continue
+#         smaller_sum = k = 0
+#         while k < len(subseq):
+#           if array[j] >= subseq[k]:
+#             smaller_sum += subseq[k]
+#             k += 1
+#           else: break
+#         # print(f"smaller_sum: {smaller_sum}, array[i]: {array[i]}, array[j]: {array[j]}, subseq[k]: {subseq[k]}")
+#         if smaller_sum > array[j]: continue
+#         else:
+#           # print('sbefore', subseq)
+#           subseq = [array[j]] + subseq[k:]
+#       # print('subseq', subseq)
 
-  return max_subsequence
+#     if sum(subseq) > max_subsequence[0]: max_subsequence = [sum(subseq), subseq]
+
+#   return max_subsequence
 
 if __name__ == "__main__":
   print(max_sum_increasing_subsequence([10, 70, 20, 30, 50, 11, 30])) # [110, [10, 20, 30, 50]]
+  print(max_sum_increasing_subsequence([-1, 1]))
+  print(max_sum_increasing_subsequence([8, 12, 2, 3, 15, 5, 7]))
+  print(max_sum_increasing_subsequence([10, 15, 4, 5, 11, 14, 31, 25, 31, 23, 25, 31, 50]))
