3cf2806b8b145ec1461956e7c9298a9693268b07
mattdepillis <mattdepillis@gmail.com>
2022-12-30 16:09:54 -0500

    initial solution for largest_range with sets -- need TC/SC

--- ae_questions/arrays/hard/largest_range/solution.py
+++ ae_questions/arrays/hard/largest_range/solution.py
@@ -2,34 +2,39 @@
 
 
 """
-# def evaluate_num(num, ranges, longest):
-#   if not len(ranges) or not num == ranges[-1][-1] + 1:
-#     ranges += [[num, num]]
-#   else: ranges[-1][1] = num
+def evaluate_num(num, ranges, longest):
+  if not len(ranges):
+    ranges += [[num, num]]
+  else:
+    if num < 0 and num == ranges[0][0] - 1:
+      ranges[0][0] = num
+    elif num >= 0 and num == ranges[-1][1] + 1:
+      ranges[-1][1] = num
+    else:
+      r = [[num, num]]
+      ranges = r + ranges if num < 0 else ranges + r
 
-#   range_diff = ranges[-1][1] - ranges[-1][0]
-#   if range_diff > longest[0]:
-#     longest = [range_diff, ranges[-1]]
-#   return longest
+  comp = ranges[0] if num < 0 else ranges[-1]
+  range_diff = abs(comp[1] - comp[0])
+  if not len(longest[1]) or range_diff > longest[0]:
+    longest = [range_diff, comp]
 
-# def largest_range(array):
-#   neg, pos = set({x for x in array if x < 0}), set({x for x in array if x >= 0})
-#   ranges, longest = [], [0, []]
-
-#   print(neg, '\n', pos)
-
-#   for num in neg: longest = evaluate_num(num, ranges, longest)
-#   print(longest)
-#   for num in pos: longest = evaluate_num(num, ranges, longest)
-  
-#   return longest[1]
+  return ranges, longest
 
 def largest_range(array):
-  return array
+  neg, pos = set({abs(x) for x in array if x < 0}), set({x for x in array if x >= 0})
+  ranges, longest = [], [0, []]
+
+  for num in neg: ranges, longest = evaluate_num(-num, ranges, longest)
+  for num in pos: ranges, longest = evaluate_num(num, ranges, longest)
+  
+  return longest[1]
 
 
 if __name__ == "__main__":
   # print(largest_range([1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]))
   # print(largest_range([1, 1, 1, 3, 4]))
   # print(largest_range([0, 9, 19, -1, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, 8, 7, 6, 15, 12, 12, 2, 1, 6, 13, 14, -23]))
-  print(largest_range([-7, -7, -7, -7, 8, -8, 0, 9, 19, -1, -3, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, -6, 8, 7, 6, 15, 12, 12, -5, 2, 1, 6, 13, 14, -4, -2]))
+  # print(largest_range([-7, -7, -7, -7, 8, -8, 0, 9, 19, -1, -3, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, -6, 8, 7, 6, 15, 12, 12, -5, 2, 1, 6, 13, 14, -4, -2]))
+  # print(largest_range([8, 4, 2, 10, 3, 6, 7, 9, 1]))
+  print(largest_range([0, -5, 9, 19, -1, 18, 17, 2, -4, -3, 10, 3, 12, 5, 16, 4, 11, 7, -6, -7, 6, 15, 12, 12, 2, 1, 6, 13, 14, -2]))
