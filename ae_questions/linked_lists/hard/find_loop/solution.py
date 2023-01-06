5642817606b647b0430d0fb0654664bf0b19337a
mattdepillis <mattdepillis@gmail.com>
2023-01-05 22:41:27 -0500

    setup code for find_loop in linkedlist data structure

---
+++ ae_questions/linked_lists/hard/find_loop/solution.py
@@ -0,0 +1,42 @@
+"""
+
+"""
+class Node:
+  def __init__(self, value, nxt):
+    self.value = value
+    self.next = nxt
+
+def find_loop(node):
+  print(node.value, node.next)
+  return node
+
+
+if __name__ == "__main__":
+  # holding data structure for nodes
+  dict = {}
+
+  nodes = [
+    {"id": "0", "next": "1", "value": 0},
+    {"id": "1", "next": "2", "value": 1},
+    {"id": "2", "next": "3", "value": 2},
+    {"id": "3", "next": "4", "value": 3},
+    {"id": "4", "next": "5", "value": 4},
+    {"id": "5", "next": "6", "value": 5},
+    {"id": "6", "next": "7", "value": 6},
+    {"id": "7", "next": "8", "value": 7},
+    {"id": "8", "next": "9", "value": 8},
+    {"id": "9", "next": "4", "value": 9}
+  ]
+
+  for node in nodes:
+    node_value, next_node = node["value"], int(node["next"])
+    dict[node_value] = Node(node_value, next_node)
+
+  for value in dict:
+    next_node = dict[value].next
+    dict[value].next = dict[next_node]
+
+  head = dict[0]
+
+  print(find_loop(head))
+
