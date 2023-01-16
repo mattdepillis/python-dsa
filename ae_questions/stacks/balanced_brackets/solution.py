7beae740c0730f7328f32ab9e3728f4abda02a4f
mattdepillis <mattdepillis@gmail.com>
2023-01-16 16:26:52 -0500

    balanced_brackets comment + complexity analysis

--- ae_questions/stacks/balanced_brackets/solution.py
+++ ae_questions/stacks/balanced_brackets/solution.py
@@ -1,5 +1,8 @@
 """
+Write a function that takes a string full of bracket characters (as well as other optional ones) and determines whether the brackets in the string are balanced.
 
+TC: O(n) -- must inspect each char in string
+SC: O(n) -- size of stack could worst-case be as big as the string itself (if all opening brackets, for example)
 """
 def balanced_brackets(string):
   stack, pairs, allowed = [], { "}": "{", ")": "(", "]": "[" }, set({ "{", "}", "(", ")", "[", "]" })
