2b6cf0b68ac6c683f9e5b647e3901c51cf075cf8
mattdepillis <mattdepillis@gmail.com>
2023-01-23 18:05:57 -0500

    solved water area

--- ae_questions/dynamic_programming/hard/water_area/solution.py
+++ ae_questions/dynamic_programming/hard/water_area/solution.py
@@ -1,9 +1,7 @@
 """
 """
 def water_area(heights):
-  direction = None
-  poles = [None, None]
-  spaces = 0
+  last = None
   max = max_index = total_pole_height = highest_since_max = 0
   area = temp_area = 0
 
@@ -11,33 +9,24 @@ def water_area(heights):
     height = heights[i]
 
     if height:
-      poles[0], poles[1] = height, poles[0]
-      d = "increasing" if not poles[1] or poles[0] >= poles[1] else "decreasing"
-
       if height >= max:
-        print('max switch!')
         area, temp_area = area + (max * (i - max_index - 1)) - total_pole_height, 0
-        print('a',  area,  'm', max, (max * (i - max_index - 1)), 't', total_pole_height)
         max, max_index = height, i
-        spaces = temp_area = total_pole_height = highest_since_max = 0
+        temp_area = total_pole_height = highest_since_max = 0
       else:
         if heights[i - 1] >= height: temp_area += 0
         else:
           if height >= highest_since_max:
             temp_area = height * (i - max_index - 1) - total_pole_height
             highest_since_max = height
-          else: temp_area += min(poles[0], poles[1]) * spaces
+          else: temp_area += min(last, height)
         total_pole_height += height
-        
-      if d != direction: spaces = 0
-      direction = d
-      spaces += 1
-
-    print(f"i: {i}, height:{height}, direction: {direction}, spaces: {spaces}, max: {max}, max_index: {max_index}, area: {area}, temp_area: {temp_area}")
+        last = height
     
-  return heights, area + temp_area
+  return area + temp_area
 
 
 if __name__ == "__main__":
   # print(water_area([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]))
-  print(water_area([0, 8, 0, 0, 10, 0, 0, 10, 0, 0, 1, 1, 0, 3]))
+  # print(water_area([0, 8, 0, 0, 10, 0, 0, 10, 0, 0, 1, 1, 0, 3]))
+  print(water_area([0, 100, 0, 0, 10, 1, 1, 10, 1, 0, 1, 1, 0, 0]))
