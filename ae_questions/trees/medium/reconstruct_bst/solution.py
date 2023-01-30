1e2e3c5ec9c00b868052d25b7c0bff9893978604
mattdepillis <mattdepillis@gmail.com>
2023-01-30 17:39:34 -0500

    reconstruct_bst comments and complexity analysis

--- ae_questions/trees/medium/reconstruct_bst/solution.py
+++ ae_questions/trees/medium/reconstruct_bst/solution.py
@@ -1,14 +1,19 @@
 """
+Given a non-empty array of ints representing the order in which a tree's nodes would be visited in a preorder traversal algorithm, write a function that inserts the nodes into a tree accoringly and returns the tree.
 
+TC: O(n) -- iterate through the nodes array
+SC: O(n) -- must create a tree with n nodes
 """
+
+# Node class
 class Node:
   def __init__(self, value):
     self.value = value
     self.left = None
     self.right = None
 
+# places each node into its correct place in the tree
 def place_node(tree, node):
-  # if not tree: tree = Node(node)
   if node < tree.value:
     if tree.left: place_node(tree.left, node)
     else: tree.left = Node(node)
@@ -18,21 +23,18 @@ def place_node(tree, node):
 
   return tree
 
-
+# preorder traversal algorithm to check work
 def preorder_traversal(node, list):
   if node: list.append(node.value)
   if node.left: preorder_traversal(node.left, list)
   if node.right: preorder_traversal(node.right, list)
   return list
 
-
+# reconstruct bst function
 def reconstruct_bst(nodes):
   tree = Node(nodes[0])
   for node in nodes[1:]:
     place_node(tree, node)
-
-  l = preorder_traversal(tree, list=[])
-  print(l)
   return tree
 
 
