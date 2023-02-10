f33ff946895fc442832025cccbddda5843fcd9f0
mattdepillis <mattdepillis@gmail.com>
2023-02-10 18:22:06 -0500

    initial solution to min_characters_for_words

--- ae_questions/strings/medium/min_characters_for_words/solution.py
+++ ae_questions/strings/medium/min_characters_for_words/solution.py
@@ -5,10 +5,29 @@ TC:
 SC:
 """
 def min_characters_for_words(words):
-  return words
+  chars, l = {}, []
+  for word in words:
+    multiple = {}
+    for letter in word:
+      if letter not in chars:
+        chars[letter] = multiple[letter] = 1
+      else:
+        if letter not in multiple: multiple[letter] = 1
+        else: multiple[letter] += 1
+    for key in multiple:
+      if multiple[key] > chars[key]: chars[key] = multiple[key]
+
+  for letter in chars:
+    for _ in range(chars[letter]):
+      l.append(letter)
+
+  return l
+
 
 
 if __name__ == "__main__":
   print(min_characters_for_words(
     ["this", "that", "did", "deed", "them!", "a"]
-  )) # ["!", "a", "d", "d", "e", "e", "h", "i", "m", "s", "t", "t"]
+  ))
+  # ["!", "a", "d", "d", "e", "e", "h", "i", "m", "s", "t", "t"]
+  # ['!', 'a', 'd', 'd', 'd', 'e', 'e', 'h', 'i', 'm', 's', 't', 't']
