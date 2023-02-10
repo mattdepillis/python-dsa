b5abe2f6c7a542bfb3ebd554f732859235c4ede1
mattdepillis <mattdepillis@gmail.com>
2023-02-10 17:53:32 -0500

    setup for min_characters_for_words

---
+++ ae_questions/strings/medium/min_characters_for_words/solution.py
@@ -0,0 +1,14 @@
+"""
+Write a function that takes an array of words and returns the minimum number of characters needed to form all of the words.
+
+TC:
+SC:
+"""
+def min_characters_for_words(words):
+  return words
+
+
+if __name__ == "__main__":
+  print(min_characters_for_words(
+    ["this", "that", "did", "deed", "them!", "a"]
+  )) # ["!", "a", "d", "d", "e", "e", "h", "i", "m", "s", "t", "t"]
