9ae954b552e3251f8668e9b5c0765b56e0b4157a
mattdepillis <mattdepillis@gmail.com>
2023-03-08 10:10:33 -0500

    initial comments + complexity  analysis for min rewards array

--- ae_questions/arrays/hard/min_rewards/solution.py
+++ ae_questions/arrays/hard/min_rewards/solution.py
@@ -1,5 +1,16 @@
 """
+Given a list of scores representing how well each student in the class did on an assignment, return the minimum total rewards required to be handed out.
 
+Rules:
+- each student must get at least reward = 1
+- a student must receive strictly more rewards than a student with a lower score next to him/her in the scores array
+- a student must receive strictly fewer rewards than a student with a higher score next to him/her in the scores array
+
+TC: O(n) - must iterate through n elements 3 separate times:
+  - get min_indices
+  - set rewards values
+  - sum the rewards
+SC: O(n) - new rewards array of size n
 """
 def min_rewards(array):
   min_indices, rewards = [], [1 for _ in range(len(array))]
@@ -17,8 +28,6 @@ def min_rewards(array):
   return sum(rewards)
   
 
-
 if __name__ == "__main__":
   print(min_rewards([8, 4, 2, 1, 3, 6, 7, 9, 5])) # 25
-
-  print(min_rewards([10, 5]))
+  print(min_rewards([10, 5])) # 3
