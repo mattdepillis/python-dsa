53396f062f0f7f91de359ca19b0076248804eca7
mattdepillis <mattdepillis@gmail.com>
2023-03-13 19:11:59 -0400

    updated code for same_bsts -- 12/13 edge cases solved

--- ae_questions/trees/hard/same_bsts/solution.py
+++ ae_questions/trees/hard/same_bsts/solution.py
@@ -8,60 +8,69 @@ def find_next_node(array, index, last_common, direction):
     direction == 'right' and array[index] > last_common
   )
 
-def recurse(one, one_index, two, two_index, tree_value, values, direction):
+def recurse(one, one_index, one_visited, two, two_index, two_visited, value, direction):
   min_index = min(one_index, two_index)
-  print(f"one: {one}, one_index: {one_index}, two: {two}, two_index: {two_index}, tree_values: {tree_value}, values: {values}, direction: {direction}")
+
+  # print(f"one: {one}, one_visited: {one_visited}, two: {two}, two_visited: {two_visited}, two_index: {two_index}, value: {value}, direction: {direction}")
+
   one_next = two_next = None
   one_next_index = two_next_index = None
+
   for i in range(min_index + 1, len(one)):
-    if not one_next and i > one_index:
-      if find_next_node(one, i, tree_value, direction):
+    if not one_next and i > one_index and not one_visited[i]:
+      if find_next_node(one, i, value, direction):
         one_next, one_next_index = one[i], i
-    if not two_next and i > two_index:
-      if find_next_node(two, i, tree_value, direction):
+    if not two_next and i > two_index and not two_visited[i]:
+      if find_next_node(two, i, value, direction):
         two_next, two_next_index = two[i], i
-    print('ot', one_next, two_next, one_next_index, two_next_index)
+
+    # print('ot', one_next, two_next, one_next_index, two_next_index)
+
     if one_next and two_next: break
 
-  # TODO: need to return values for 
+  # print("one and two next:", one_next, two_next)
 
-  print("one and two next:", one_next, two_next)
   if one_next == two_next:
-    if not one_next: return True, None
-    is_same, val = recurse(
-      one, one_next_index, two, two_next_index, one_next, values + [one_next],'left'
+    if not one_next: return True
+    
+    one_visited[one_next_index], two_visited[two_next_index] = True, True
+    
+    return recurse(
+      one, one_index, one_visited, two, two_index, two_visited, one_next, 'left'
+    ) and recurse(
+      one, one_index, one_visited, two, two_index, two_visited, one_next, 'right'
     )
-    if is_same and not val: return True
-    return is_same and recurse(
-      one, one_next_index, two, two_next_index, one_next, values + [one_next],'right'
-    )
-
+  return False
+  
+def generate_visited(array):
+  return [True] + [False for _ in range(len(array) - 1)]
 
 def same_bsts(one, two):
   if not len(one) == len(two) or not one[0] == two[0]:
     return False
   
-  one_stack, two_stack, common, vals = [0], [0], one[0], [one[0]]
-  
-  # return recurse(
-  #   one, one_stack[0], two, two_stack[0], common, 'left'
-  # ) and recurse(
-  #   one, one_stack[0], two, two_stack[0], common, 'right'
-  # )
+  one_visited, two_visited = generate_visited(one), generate_visited(two)
 
-  left_same, val = recurse(
-    one, one_stack[0], two, two_stack[0], common, vals, 'left'
+  one_index, two_index, common = 0, 0, one[0]
+  
+  return recurse(
+    one, one_index, one_visited, two, two_index, two_visited, common, 'left'
+  ) and recurse(
+    one, one_index, one_visited, two, two_index, two_visited, common, 'right'
   )
-  print('LEFT', left_same)
-  if left_same and not val:
-    right_same, val = recurse(
-      one, one_stack[0], two, two_stack[0], common, vals, 'right'
-    )
-    return right_same
-  return False
 
 if __name__ == "__main__":
+  # print(same_bsts(
+  #   [10, 15, 8, 12, 94, 81, 5, 2, 10],
+  #   [10, 8, 5, 15, 2, 10, 12, 94, 81]
+  # )) # False
+  # print(same_bsts(
+  #   [10, 15, 8, 12, 94, 81, 5, 2, 11],
+  #   [10, 8, 5, 15, 2, 12, 11, 94, 81]
+  # )) # True
+
+  # TODO get this to return True
   print(same_bsts(
-    [10, 15, 8, 12, 94, 81, 5, 2, 10],
-    [10, 8, 5, 15, 2, 10, 12, 94, 81]
+    [10, 15, 8, 12, 94, 81, 5, 2, -1, 100, 45, 12, 8, -1, 8, 2, -34],
+    [10, 8, 5, 15, 2, 12, 94, 81, -1, -1, -34, 8, 2, 8, 12, 45, 100]
   ))
