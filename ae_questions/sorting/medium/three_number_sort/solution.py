96364f78f17694d1f15215130ac224c278d3cc5d
mattdepillis <mattdepillis@gmail.com>
2023-04-14 00:21:56 -0400

    fixed small bug in three_number_sort

--- ae_questions/sorting/medium/three_number_sort/solution.py
+++ ae_questions/sorting/medium/three_number_sort/solution.py
@@ -12,24 +12,28 @@ def three_number_sort(array, order):
   for i in range(len(order) - 1):
     for j in range(len(array)):
       if order[i] == array[j]:
-        if j == placed: continue
         array[placed], array[j] = array[j], array[placed]
         placed += 1
   return array
 
 
 if __name__ == "__main__":
-  print(three_number_sort(
-    [1, 0, 0, -1, -1, 0, 1, 1],
-    [0, 1, -1]
-  )) # [0, 0, 0, 1, 1, 1, -1, -1]
+  # print(three_number_sort(
+  #   [1, 0, 0, -1, -1, 0, 1, 1],
+  #   [0, 1, -1]
+  # )) # [0, 0, 0, 1, 1, 1, -1, -1]
 
   # print(three_number_sort(
   #   [0, 1],
   #   [1, 2, 0]
   # ))
 
+  # print(three_number_sort(
+  #   [9, 9, 9, 7, 9, 7, 9, 9, 7, 9],
+  #   [7, 11, 9]
+  # ))
+
   print(three_number_sort(
-    [9, 9, 9, 7, 9, 7, 9, 9, 7, 9],
-    [7, 11, 9]
+    [-2, -3, -3, -3, -3, -3, -2, -2, -3],
+    [-2, -3, 0]
   ))
