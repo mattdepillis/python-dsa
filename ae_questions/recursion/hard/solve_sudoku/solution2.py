11b0ff861e06cc2a4ec1843ba0657ece66faec13
mattdepillis <mattdepillis@gmail.com>
2023-04-21 09:03:51 -0400

    code for full sudoku solution

---
+++ ae_questions/recursion/hard/solve_sudoku/solution2.py
@@ -0,0 +1,58 @@
+"""
+
+"""
+
+def is_valid(board, row_index, col_index, digit):
+  if digit in board[row_index]: return False
+  if digit in map(lambda r: r[col_index], board): return False
+
+  row_start = 0 if row_index <= 2 else 3 if row_index <= 5 else 6
+  col_start = 0 if col_index <= 2 else 3 if col_index <= 5 else 6
+
+  square = [board[i][j]
+    for i in range(row_start, row_start + 3)
+    for j in range(col_start, col_start + 3)
+  ]
+  if digit in square: return False
+
+  return True
+
+
+def solve(board, row, col):
+  if row == 9: return board, True
+
+  if board[row][col] != 0: return solve(
+    board,
+    row if col < 8 else row + 1,
+    col + 1 if col < 8 else 0
+  )
+ 
+  for i in range(1, 10):
+    if is_valid(board, row, col, i):
+      board[row][col] = i
+      if solve(
+        board,
+        row if col < 8 else row + 1,
+        col + 1 if col < 8 else 0
+      ): return True
+    
+  board[row][col] = 0
+  return False
+
+def solve_sudoku(board):
+  solve(board, 0, 0)
+  return board
+
+
+if __name__ == "__main__":
+  print(solve_sudoku([
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+  ]))
