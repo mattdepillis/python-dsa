a1aa9b95319d9479363a0996d0d66e93da945d4c
mattdepillis <mattdepillis@gmail.com>
2023-04-25 09:37:15 -0400

    initial code for minheap construction - buildHeap() and siftDown()

---
+++ ae_questions/heaps/medium/min_heap_construction/solution.py
@@ -0,0 +1,54 @@
+"""
+
+"""
+class MinHeap:
+  def __init__(self, array):
+    # Do not edit the line below.
+    self.heap = self.buildHeap(array)
+
+  def buildHeap(self, array):
+    """
+    """
+    last_parent = (len(array) - 2) // 2
+    for i in reversed(range(0, last_parent + 1)):
+      array = self.siftDown(i, array)
+    return array
+
+  def siftDown(self, i, array):
+    """
+    """
+    if i > (len(array) - 2) // 2: return array
+
+    if (2 * i) + 2 > len(array) - 1:
+      min_child_index = (2 * i) + 1
+    else:
+      min_child_index = (2 * i) + 1 if (
+        array[(2 * i) + 1] < array[(2 * i) + 2]
+      ) else (2 * i) + 2
+
+    if array[min_child_index] < array[i]:
+      array[i], array[min_child_index] = array[min_child_index], array[i]
+      array = self.siftDown(min_child_index, array)
+
+    return array
+
+  def siftUp(self):
+    # Write your code here.
+    pass
+
+  def peek(self):
+    # Write your code here.
+    pass
+
+  def remove(self):
+    # Write your code here.
+    pass
+
+  def insert(self, value):
+    # Write your code here.
+    pass
+
+
+if __name__ == "__main__":
+  heap = MinHeap([48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41])
+  print(heap.heap)
