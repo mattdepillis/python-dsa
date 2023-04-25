5a0612221cc187dc0077c792d8d99fa785a64fc6
mattdepillis <mattdepillis@gmail.com>
2023-04-25 10:07:16 -0400

    working minheap, but need to refactor to pass ae tests -- 1 arg siftDown()

--- ae_questions/heaps/medium/min_heap_construction/solution.py
+++ ae_questions/heaps/medium/min_heap_construction/solution.py
@@ -1,6 +1,8 @@
 """
 
 """
+import math
+
 class MinHeap:
   def __init__(self, array):
     # Do not edit the line below.
@@ -11,47 +13,60 @@ class MinHeap:
     Builds the heap, represented as a list, from an unsorted list.
     Finds the last current parent node and sifts all parents down.
     """
+    self.heap = array
     last_parent = (len(array) - 2) // 2
     for i in reversed(range(0, last_parent + 1)):
-      array = self.siftDown(i, array)
-    return array
+      self.siftDown(i)
+    return self.heap
 
-  def siftDown(self, i, array):
+  def siftDown(self, i):
     """
     Recursively sifts a parent node down if it's greater than its smallest child.
     """
-    if i > (len(array) - 2) // 2: return array
+    if i > (len(self.heap) - 2) // 2: return
 
-    if (2 * i) + 2 > len(array) - 1:
+    if (2 * i) + 2 > len(self.heap) - 1:
       min_child_index = (2 * i) + 1
     else:
       min_child_index = (2 * i) + 1 if (
-        array[(2 * i) + 1] < array[(2 * i) + 2]
+        self.heap[(2 * i) + 1] < self.heap[(2 * i) + 2]
       ) else (2 * i) + 2
 
-    if array[min_child_index] < array[i]:
-      array[i], array[min_child_index] = array[min_child_index], array[i]
-      array = self.siftDown(min_child_index, array)
+    if self.heap[min_child_index] < self.heap[i]:
+      self.heap[i], self.heap[min_child_index] = self.heap[min_child_index], self.heap[i]
+      self.siftDown(min_child_index)
 
-    return array
+  def siftUp(self, i):
+    parent_index = math.floor((i - 1) // 2)
 
-  def siftUp(self):
-    # Write your code here.
-    pass
+    if self.heap[i] < self.heap[parent_index]:
+      self.heap[i], self.heap[parent_index] = self.heap[parent_index], self.heap[i]
+      self.siftUp(parent_index)
 
   def peek(self):
-    # Write your code here.
-    pass
+    return self.heap[0]
 
   def remove(self):
-    # Write your code here.
-    pass
+    last = len(self.heap) - 1 
+    self.heap[0], self.heap[last] = self.heap[last], self.heap[0]
+    self.heap.pop(last)
+    self.siftDown(0)
 
   def insert(self, value):
-    # Write your code here.
-    pass
+    self.heap.append(value)
+    self.siftUp(len(self.heap) - 1)
 
 
 if __name__ == "__main__":
   heap = MinHeap([48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41])
+  print(heap.heap)
+  heap.insert(76)
+  print(heap.peek())
+  heap.remove()
+  print(heap.heap)
+  print(heap.peek())
+  heap.remove()
+  print(heap.heap)
+  print(heap.peek())
+  heap.insert(87)
   print(heap.heap)
