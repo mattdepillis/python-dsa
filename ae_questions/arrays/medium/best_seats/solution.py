44603ed86a804c28eafc13d219c0bf796b23137c
mattdepillis <mattdepillis@gmail.com>
2023-04-26 09:27:55 -0400

    slight mod to best_seat

--- ae_questions/arrays/medium/best_seats/solution.py
+++ ae_questions/arrays/medium/best_seats/solution.py
@@ -10,7 +10,7 @@ SC: O(1) -- consistent variable assignment for any n values
 
 def best_seats(array):
   last_taken = most_consecutive = 0
-  best_seat = None
+  best_seat = -1
 
   for i in range(last_taken, len(array)):
     if array[i] != 0:
@@ -19,7 +19,7 @@ def best_seats(array):
         best_seat = last_taken + (i - last_taken) // 2
       last_taken = i
 
-  return -1 if best_seat is None else best_seat
+  return best_seat
 
 
 if __name__ == "__main__":
