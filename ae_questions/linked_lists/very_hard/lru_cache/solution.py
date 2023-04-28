0c5bc5541e23fa0eed4dbb342190b4cea875ef41
mattdepillis <mattdepillis@gmail.com>
2023-04-28 18:53:14 -0400

    init linked_lists -- LRUCache file

---
+++ ae_questions/linked_lists/very_hard/lru_cache/solution.py
@@ -0,0 +1,23 @@
+"""
+
+
+"""
+class LRUCache:
+  def __init__(self, maxSize):
+    self.maxSize = maxSize or 1
+
+  def insertKeyValuePair(self, key, value):
+    # Write your code here.
+    pass
+
+  def getValueFromKey(self, key):
+    # Write your code here.
+    pass
+
+  def getMostRecentKey(self):
+    # Write your code here.
+    pass
+
+
+if __name__ == "__main__":
+  c = LRUCache(3)
