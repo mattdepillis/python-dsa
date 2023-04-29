f29737799ef6ae4de1b2e8ab1539a33b08aa18a5
mattdepillis <mattdepillis@gmail.com>
2023-04-29 12:30:11 -0400

    LRUCache - traversem trim_cache, insert methods

--- ae_questions/linked_lists/very_hard/lru_cache/solution.py
+++ ae_questions/linked_lists/very_hard/lru_cache/solution.py
@@ -2,13 +2,37 @@
 
 
 """
+class Node:
+  def __init__(self, k, v):
+    self.key = k
+    self.value = v
+    self.next = None
+
 class LRUCache:
   def __init__(self, maxSize):
     self.maxSize = maxSize or 1
+    self.root = None
+
+  def traverse(self):
+    curr = self.root
+    while curr:
+      print(curr.key, curr.value)
+      curr = curr.next
+
+  def trim_cache(self):
+    curr, count = self.root, 0
+    while curr.next:
+      count += 1
+      if count == 3: curr.next = None
+      else: curr = curr.next
 
   def insertKeyValuePair(self, key, value):
-    # Write your code here.
-    pass
+    if not self.root:
+      self.root = Node(key, value)
+    else:
+      nxt, self.root = self.root, Node(key, value)
+      self.root.next = nxt
+      self.trim_cache()
 
   def getValueFromKey(self, key):
     # Write your code here.
@@ -21,3 +45,9 @@ class LRUCache:
 
 if __name__ == "__main__":
   c = LRUCache(3)
+  c.insertKeyValuePair("d", 4)
+  c.insertKeyValuePair("c", 3)
+  c.insertKeyValuePair("b", 2)
+  c.insertKeyValuePair("a", 1)
+  c.insertKeyValuePair("g", 7)
+  c.traverse()
