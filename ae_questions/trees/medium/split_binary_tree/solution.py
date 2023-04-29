bd56b03e62dc72c59c1832121dea59de69f685da
mattdepillis <mattdepillis@gmail.com>
2023-04-29 14:33:53 -0400

    split_binary_tree setup

---
+++ ae_questions/trees/medium/split_binary_tree/solution.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+class BinaryTree:
+  """ Binary tree node class. """
+  def __init__(self, value, left=None, right=None):
+      self.value = value
+      self.left = left
+      self.right = right
+
+def split_binary_tree(tree):
+  return tree
+
+
+if __name__ == "__main__":
+  """ Test Case 1 """
+  r = BinaryTree(1)
+  print(split_binary_tree(r))
