ccc2437ad1b1ebf5fd477d1446040a8aed8b22e2
mattdepillis <mattdepillis@gmail.com>
2023-05-06 18:51:55 -0400

    construct_local_tree method for generating tree from list of nodes locally

--- ae_questions/trees/medium/split_binary_tree/solution.py
+++ ae_questions/trees/medium/split_binary_tree/solution.py
@@ -8,11 +8,37 @@ class BinaryTree:
       self.left = left
       self.right = right
 
+def construct_local_tree(nodes):
+  tree_nodes, root = {}, None
+  for i in reversed(range(len(nodes))):
+    node = nodes[i]
+    val, left, right, id = node['value'], node['left'], node['right'], node['id']
+    new_node = BinaryTree(
+      val,
+      tree_nodes[left] if left else left,
+      tree_nodes[right] if right else right
+    )
+    if i == 0: root = new_node
+    tree_nodes[id] = new_node
+  return root
+
+def find_total_tree_sum(tree):
+  return tree
+
 def split_binary_tree(tree):
   return tree
 
 
 if __name__ == "__main__":
   """ Test Case 1 """
-  r = BinaryTree(1)
-  print(split_binary_tree(r))
+
+  nodes = [
+    {"id": "1", "left": "2", "right": "3", "value": 1},
+    {"id": "2", "left": "4", "right": "5", "value": 2},
+    {"id": "3", "left": None, "right": "7", "value": 3},
+    {"id": "4", "left": None, "right": None, "value": 4},
+    {"id": "5", "left": None, "right": None, "value": 5},
+    {"id": "7", "left": None, "right": None, "value": 7}
+  ]
+
+  root = construct_local_tree(nodes)
