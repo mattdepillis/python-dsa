4018dc1098c6146b3042ced094bc1c8ce4173135
mattdepillis <mattdepillis@gmail.com>
2023-05-10 19:21:09 -0400

    missing_numbers (list) initial commit

---
+++ ae_questions/arrays/medium/missing_numbers/solution.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+def missing_numbers(nums):
+  return nums
+
+
+if __name__ == "__main__":
+  # print(missing_numbers(
+  #   [1, 15, 16, 17, 18, 19, 20, 21, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  # )) # 1, 2
+
+  print(missing_numbers(
+    [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  ))
