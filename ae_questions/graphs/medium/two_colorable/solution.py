eb2e5b1b4681dcefc4296ef7bac1219700f8dfe4
mattdepillis <mattdepillis@gmail.com>
2023-05-11 19:17:07 -0400

    rough solution for two_colorable

--- ae_questions/graphs/medium/two_colorable/solution.py
+++ ae_questions/graphs/medium/two_colorable/solution.py
@@ -2,12 +2,31 @@
 
 """
 def two_colorable(edges):
-  return edges
+  vertices, colors = {}, ["bl", "gr"]
+
+  for edge in range(len(edges)):
+    if not edge in vertices: vertices[edge] = colors[0]
+
+    neighbor_color_index = colors.index(vertices[edge]) - 1
+
+    for neighbor in edges[edge]:
+      if not neighbor in vertices: vertices[neighbor] = colors[neighbor_color_index]
+      elif not vertices[neighbor] == colors[neighbor_color_index]: return False
+
+  return True
 
 
 if __name__ == "__main__":
+  # print(two_colorable([
+  #   [1, 2],
+  #   [0, 2],
+  #   [0, 1]
+  # ]))
+
   print(two_colorable([
-    [1, 2],
-    [0, 2],
-    [0, 1]
+    [1, 4],
+    [0, 2, 3],
+    [1, 4],
+    [1],
+    [0, 2]
   ]))
