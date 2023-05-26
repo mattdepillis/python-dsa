f35c280fb9592dddd535f09edd0b10f5ec3a1bae
mattdepillis <mattdepillis@gmail.com>
2023-05-26 19:22:36 +0300

    13/20 cases - need to try new method

--- ae_questions/dynamic_programming/hard/maximize_expression/solution.py
+++ ae_questions/dynamic_programming/hard/maximize_expression/solution.py
@@ -2,27 +2,40 @@
 
 """
 def find_val(array, r, pos):
+  print("r:", r)
   if pos:
     i, m = None, float('-inf')
-    for idx in r: i, m = idx, array[idx] if array[idx] > m else i, m
+    for idx in r: (i, m) = (idx, array[idx]) if array[idx] > m else (i, m)
   else:
     i, m = None, float('inf')
-    for idx in r: i, m = idx, array[idx] if array[idx] < m else i, m
+    for idx in r:
+      print(array[idx], array[idx] < m)
+      (i, m) = (idx, array[idx]) if array[idx] < m else (i, m)
   return i, m
 
 def extend_expression(array, sum, start, remaining):
-  # keep track of index and value
+  print(f"""
+    array: {array},
+    sum: {sum},
+    start: {start},
+    remaining: {remaining}
+  """)
 
-  possible_values = range(start, len(array) - remaining)
+  # keep track of index and value
+  to_add = remaining % 2 == 0
+
+  possible_values = range(start, len(array) - remaining + 1)
   if len(possible_values) == remaining:
     i = possible_values[0]
     val = array[i]
   else:
-    i, val = find_val(array, possible_values, True) if remaining % 2 == 0 else find_val(array, possible_values, False)
+    i, val = find_val(array, possible_values, True) if to_add else find_val(array, possible_values, False)
 
-  
+  print(f"i: {i}, val: {val}")
 
-  return possible_values
+  sum = sum + val if to_add else sum - val
+
+  return extend_expression(array, sum, i + 1, remaining - 1) if remaining > 1 else sum
 
 def maximize_expression(array):
   max_exp = 0
@@ -32,6 +45,10 @@ def maximize_expression(array):
 
 
 if __name__ == "__main__":
+  # print(maximize_expression(
+  #   [3, 6, 1, -3, 2, 7]
+  # )) # 4
+
   print(maximize_expression(
-    [3, 6, 1, -3, 2, 7]
-  )) # 4
+    [6, 2, 3, 4, 1, -1, -2, 3, 1, 7, 8, -8, 2, 5, 1]
+  ))
