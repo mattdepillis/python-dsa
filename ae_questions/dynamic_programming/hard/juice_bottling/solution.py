b77ae63a51908257e84bbd9da95add4c6261315e
mattdepillis <mattdepillis@gmail.com>
2023-05-30 19:34:03 +0300

    setup for juice_bottling (dynamic programming)

---
+++ ae_questions/dynamic_programming/hard/juice_bottling/solution.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+def juice_bottling(prices):
+  units = len(prices) - 1
+
+  return units
+
+
+if __name__ == "__main__":
+  print(juice_bottling(
+    [0, 1, 3, 2]
+  )) # [1, 2]
+
+  # print(juice_bottling(
+  #   [0, 2, 3, 4]
+  # )) # [1, 1, 1]
