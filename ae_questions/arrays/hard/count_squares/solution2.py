5a86440d615650ff7708174b7c0ac410474d5ed7
mattdepillis <mattdepillis@gmail.com>
2023-05-31 09:41:35 -0400

    original soln missed a test case - new file for solution2

---
+++ ae_questions/arrays/hard/count_squares/solution2.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+def count_squares(points):
+  return points
+
+if __name__ == "__main__":
+  print(count_squares([
+    [1, 1],
+    [0, 0],
+    [-4, 2],
+    [-2, -1],
+    [0, 1],
+    [1, 0],
+    [-1, 4]
+  ]))
