be1128149bb0db41c54c1a4a10a22afa1fe9c869
mattdepillis <mattdepillis@gmail.com>
2023-06-01 18:23:19 -0400

    initial code for generate_div_tags -- 3/6 cases

--- ae_questions/recursion/hard/generate_div_tags/solution.py
+++ ae_questions/recursion/hard/generate_div_tags/solution.py
@@ -1,11 +1,25 @@
 """
 
 """
-def recurse():
-  return
+def generate_new_tag_combos(string):
+  return [
+    "<div>" + string + "</div>", # wrap
+    "<div></div>" + string, # prepend
+    string + "<div></div>" # append
+  ]
+
+def recurse(tags):
+  if tags == 1: return [ "<div></div>" ]
+  valid, prior_valid = set(), recurse(tags - 1)
+  for combo in prior_valid:
+    new_combos = generate_new_tag_combos(combo)
+    for nc in new_combos:
+      if nc in valid: continue
+      valid.add(nc)
+  return list(valid)
 
 def generate_div_tags(tags):
-  return recurse()
+  return recurse(tags)
 
 
 if __name__ == "__main__":
