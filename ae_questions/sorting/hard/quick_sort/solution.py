588b3be9f078411feaddd6e30e64947cc7ef58b1
mattdepillis <mattdepillis@gmail.com>
2023-06-21 22:05:20 -0400

    fully working quick sort solution

--- ae_questions/sorting/hard/quick_sort/solution.py
+++ ae_questions/sorting/hard/quick_sort/solution.py
@@ -11,19 +11,23 @@ def quick_sort(array):
     if array[left] >= array[pointer] and array[right] < array[pointer]:
       array[left], array[right] = array[right], array[left]
       right -= 1
+    elif array[right] >= array[pointer]: right -= 1
     else: left += 1
 
   print('a', array, right)
 
   if array[right] < array[pointer]:
     array[pointer], array[right] = array[right], array[pointer]
+    pointer = right
 
-  if right > (len(array) - 1) - right:
-    array[right + 1:] = quick_sort(array[right + 1:])
-    array[:right] = quick_sort(array[:right])
+  print('p', pointer, array[pointer], array)
+
+  if pointer > (len(array) - 1) - pointer:
+    array[pointer + 1:] = quick_sort(array[pointer + 1:])
+    array[:pointer] = quick_sort(array[:pointer])
   else:
-    array[:right] = quick_sort(array[:right])
-    array[right + 1:] = quick_sort(array[right + 1:])
+    array[:pointer] = quick_sort(array[:pointer])
+    array[pointer + 1:] = quick_sort(array[pointer + 1:])
 
   return array
 
@@ -31,4 +35,6 @@ def quick_sort(array):
 if __name__ == "__main__":
   # print(quick_sort([7, 8, 5, 2, 9, 5, 6, 3]))
 
-  print(quick_sort([1, 3, 2]))
+  # print(quick_sort([1, 3, 2]))
+
+  print(quick_sort([5, -2, 2, -8, 3, -10, -6, -1, 2, -2, 9, 1, 1]))
