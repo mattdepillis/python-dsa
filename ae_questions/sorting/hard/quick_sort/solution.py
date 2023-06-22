948a0f3e23c4a4b6a4f5b34c410beec036f7e706
mattdepillis <mattdepillis@gmail.com>
2023-06-21 22:29:00 -0400

    comments, complexity analysis for quick sort

--- ae_questions/sorting/hard/quick_sort/solution.py
+++ ae_questions/sorting/hard/quick_sort/solution.py
@@ -1,5 +1,11 @@
 """
+Write a QuickSort algorithm to sort an input array of integers, from smallest to largest.
 
+TC:
+  - Best/Average: O(n * log(n)) -- iterate through log(n) elements on average (split of array into L and R), n times
+  - Worst: O(n^2) -- array is already perfectly sorted or totally reversed
+SC:
+  - O(log(n)) -- we hold at maximum log(n) to n (but average = log(n)) calls on the stack at a given time due to partitioning of array and recursion
 """
 def quick_sort(array):
   if len(array) < 2: return array
@@ -14,13 +20,9 @@ def quick_sort(array):
     elif array[right] >= array[pointer]: right -= 1
     else: left += 1
 
-  print('a', array, right)
-
   if array[right] < array[pointer]:
     array[pointer], array[right] = array[right], array[pointer]
     pointer = right
-
-  print('p', pointer, array[pointer], array)
 
   if pointer > (len(array) - 1) - pointer:
     array[pointer + 1:] = quick_sort(array[pointer + 1:])
