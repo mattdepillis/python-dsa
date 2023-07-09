061c475eb63ac9410c4c4577b06b1f03a4ab11a3
mattdepillis <mattdepillis@gmail.com>
2023-07-09 15:06:52 -0400

    solution for majority element

---
+++ ae_questions/arrays/medium/majority_element/solution.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+def majority_element(array):
+  i = last_element = 0
+  while i < len(array) / 2:
+    counter, element = 0, array[i]
+    if not element == last_element:
+      for j in range(i, len(array)):
+        counter = counter + 1 if array[j] == element else counter - 1
+      if counter > 0: return element
+    last_element = element
+    i = i + 1
+
+
+if __name__ == "__main__":
+  print(majority_element([1, 2, 2, 2, 1]))
