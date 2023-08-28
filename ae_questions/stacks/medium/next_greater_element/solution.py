8a66caf4f8623f7c6305a851d047be66161f97ca
mattdepillis <mattdepillis@gmail.com>
2023-08-28 19:11:13 -0400

    slight refactoring - fn for looking backward in stack

--- ae_questions/stacks/medium/next_greater_element/solution.py
+++ ae_questions/stacks/medium/next_greater_element/solution.py
@@ -3,21 +3,31 @@
 """
 def look_forward(stack, array, index, final, max_val):
   stack.append(array[index])
-  stack_start = final.index(None)
 
   if array[index] > max_val:
     max_val = array[index]
-  if len(stack) > 1:
-    iter, stack_position = index - 1, len(stack) - 1
-    while iter >= stack_start:
-      if not final[iter]:
-        stack_position -= 1
-        if stack[stack_position] < array[index]:
-          stack.pop(stack_position)
-          final[iter] = array[index]
-      iter -= 1
+  
+  iter, stack_start, stack_position = index - 1, final.index(None), len(stack) - 1
+  while iter >= stack_start:
+    if not final[iter]:
+      stack_position -= 1
+      if stack[stack_position] < array[index]:
+        stack.pop(stack_position)
+        final[iter] = array[index]
+    iter -= 1
 
   return stack, final, max_val
+
+def look_backward(stack, index, final, start_val):
+  if not final[index]:
+    if len(stack) == 1:
+      final[index] = -1
+    else:
+      if stack[len(stack) - 1] < start_val: final[index] = start_val
+      else: final[index] = final[0]
+      stack.pop(len(stack) - 1)
+
+  return stack, final
 
 def next_greater_element(arr):
   final = [None for _ in arr]
@@ -28,16 +38,7 @@ def next_greater_element(arr):
     (stack, final, max_val) = look_forward(stack, arr, i, final, max_val)
 
   for j in reversed(range(len(final))):
-    if not final[j]:
-      if len(stack) < 2:
-        final[j] = -1
-        break
-      else:
-        if stack[len(stack) - 1] < start_val: final[j] = start_val
-        else: final[j] = final[0]
-        stack.pop(len(stack) - 1)
-  
-  print(max_val, stack)
+    (stack, final) = look_backward(stack, j, final, start_val)
 
   return final
 
