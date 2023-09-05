7ac2a85066c8398dce0e1c8afe25b875ce20d63c
mattdepillis <mattdepillis@gmail.com>
2023-09-04 23:17:04 -0400

    comments, tc and sC

--- ae_questions/stacks/medium/reverse_polish_notation/solution.py
+++ ae_questions/stacks/medium/reverse_polish_notation/solution.py
@@ -1,5 +1,8 @@
 """
+Given a list of string 'tokens' that represent numbers or mathematical operators (+, -, *, /), return the value of the mathematical expression using Reverse Polish Notation. RPN is when an operator follows two integers (e.g. 2 2 + means to add 2 and 2).
 
+TC: O(n) -- loop through the elements
+SC: O(n) -- max n elements in the stack at a given time
 """
 def is_operator(element):
   return element in [ "+", "-", "*", "/" ]
