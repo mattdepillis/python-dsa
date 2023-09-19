832226feacc80fc1fb6ab31284442c3131140fb2
mattdepillis <mattdepillis@gmail.com>
2023-09-18 23:05:47 -0400

    heap sort starter code

---
+++ ae_questions/sorting/hard/heap_sort/solution.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+def heap_sort(array):
+
+  for i in range(len(array)):
+    print(array[i])
+
+  return array
+
+
+if __name__ == "__main__":
+  print(heap_sort(
+    [8, 5, 2, 9, 5, 6, 3]
+  ))
