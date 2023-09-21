16cbefb7febc8df7610dd4144f42666dbe7d9887
mattdepillis <mattdepillis@gmail.com>
2023-09-21 19:03:34 -0400

    full solution for heap sort

--- ae_questions/sorting/hard/heap_sort/solution.py
+++ ae_questions/sorting/hard/heap_sort/solution.py
@@ -3,14 +3,15 @@
 """
 def heap_sort(array):
   for i in range(len(array) - 1):
-    first_child_index = ((len(array) - i) // 2)
+    top_of_heap = i
+    last_parent_index = ((len(array) - i) // 2) - 1 + i
 
-    for j in reversed(range(0, first_child_index)):
-      children = [(2 * j) + 1, (2 * j) + 2]
+    for j in reversed(range(top_of_heap, last_parent_index + 1)):
+      children = [(2 * j + 1 - i), (2 * j + 2 - i)]
 
       if children[1] >= len(array):
         children.pop(1)
-      
+
       greatest_child_index = children[0] if (
         len(children) < 2 or array[children[0]] > array[children[1]]
       ) else children[1]
@@ -20,7 +21,7 @@ def heap_sort(array):
       if greatest_child > array[j]:
         array[j], array[greatest_child_index] = array[greatest_child_index], array[j]
 
-  return array
+  return array[::-1]
 
 
 if __name__ == "__main__":
