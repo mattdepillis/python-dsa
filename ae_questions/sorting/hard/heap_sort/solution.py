2cf7d45689f13a2feaf4b00646432bb2ee3e7de3
mattdepillis <mattdepillis@gmail.com>
2023-09-20 22:59:21 -0400

    sorted greatest-to-largest - need to reverse to pass test cases

--- ae_questions/sorting/hard/heap_sort/solution.py
+++ ae_questions/sorting/hard/heap_sort/solution.py
@@ -2,9 +2,23 @@
 
 """
 def heap_sort(array):
+  for i in range(len(array) - 1):
+    first_child_index = ((len(array) - i) // 2)
 
-  for i in range(len(array)):
-    print(array[i])
+    for j in reversed(range(0, first_child_index)):
+      children = [(2 * j) + 1, (2 * j) + 2]
+
+      if children[1] >= len(array):
+        children.pop(1)
+      
+      greatest_child_index = children[0] if (
+        len(children) < 2 or array[children[0]] > array[children[1]]
+      ) else children[1]
+
+      greatest_child = array[greatest_child_index]
+
+      if greatest_child > array[j]:
+        array[j], array[greatest_child_index] = array[greatest_child_index], array[j]
 
   return array
 
