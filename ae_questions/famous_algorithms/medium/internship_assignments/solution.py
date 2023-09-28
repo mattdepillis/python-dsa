153f80290b0fab9c42c7fc55c700d3864f14923f
mattdepillis <mattdepillis@gmail.com>
2023-09-27 21:18:04 -0400

    setup code for internship_assignments

---
+++ ae_questions/famous_algorithms/medium/internship_assignments/solution.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+def internship_assignments(interns, teams):
+  return
+
+
+if __name__ == "__main__":
+  print(internship_assignments(
+    [
+      [0, 1, 2],
+      [1, 0, 2],
+      [1, 2, 0]
+    ], # interns
+    [
+      [2, 1, 0],
+      [1, 2, 0],
+      [0, 2, 1]
+    ] # teams
+  ))
