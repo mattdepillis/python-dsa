04b38ebd4edc282eed2535bd4e1119bc2ad1f001
mattdepillis <mattdepillis@gmail.com>
2023-10-05 22:35:38 -0400

    couldn't come up with optimal solution

--- ae_questions/dynamic_programming/hard/dice_throws/solution.py
+++ ae_questions/dynamic_programming/hard/dice_throws/solution.py
@@ -1,27 +1,25 @@
 """
 
 """
-def recursive_determine_dice_throws(total_dice, remaining_dice, sides, target, prev, valid_combos):
-    
-    for value in range(1, sides + 1):
-        adjusted_target = target - value
-        if adjusted_target == 0 and remaining_dice > 0:
-            prev.append(value)
+def recursive_determine_dice_throws(remaining_dice, sides, target, prev):
+    valid_combos = []
 
-            return prev
-        elif adjusted_target < 0 or remaining_dice <= 0:
-            break
-        else:
-            vc = recursive_determine_dice_throws(
-                total_dice, remaining_dice - 1, sides, adjusted_target, prev + [value], valid_combos
+    # print(f"remaining_dice: {remaining_dice}, target: {target}, prev: {prev}")
+
+    for val in range(1, sides + 1):
+        if target - val > 0 and remaining_dice > 0:
+            valid_combos += recursive_determine_dice_throws(
+                remaining_dice - 1, sides, target - val, prev + [val]
             )
-            if len(vc) > 0 and isinstance(vc[0], int) and len(vc) == total_dice: valid_combos.append(vc)
-        print(valid_combos, prev, value, remaining_dice)
+        else:
+            if target - val == 0 and remaining_dice == 1: valid_combos.append(prev + [val])
+            break
     
     return valid_combos
 
+
 def dice_throws(num_dice, num_sides, target):
-    valid_combos = recursive_determine_dice_throws(num_dice, num_dice, num_sides, target, [], [])
+    valid_combos = recursive_determine_dice_throws(num_dice, num_sides, target, [])
     print(valid_combos)
     return len(valid_combos)
 
@@ -30,6 +28,10 @@ if __name__ == "__main__":
     #     num_dice=2, num_sides=6, target=7
     # ))
 
+    # print(dice_throws(
+    #     num_dice=3, num_sides=10, target=12
+    # ))
+
     print(dice_throws(
-        num_dice=3, num_sides=10, target=12
+        num_dice=11, num_sides=9, target=32
     ))
