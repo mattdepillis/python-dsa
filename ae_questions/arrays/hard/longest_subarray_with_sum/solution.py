8be8301d99cba0aad7252f0cb7967f7312def511
mattdepillis <mattdepillis@gmail.com>
2023-11-01 19:48:31 -0400

    setup for longest_subarray_with_sum (lists)

---
+++ ae_questions/arrays/hard/longest_subarray_with_sum/solution.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+def longest_subarray_with_sum(array, target):
+    return
+
+
+if __name__ == "__main__":
+    print(longest_subarray_with_sum(
+        [1, 2, 3, 4, 3, 3, 1, 2, 1],
+        10
+    )) # [4, 8]
