d811120621a5dec5fd592dbabcd0da480f9f37aa
mattdepillis <mattdepillis@gmail.com>
2023-11-01 20:09:52 -0400

    initial code for solution -- 3/4 of test cases passed

--- ae_questions/arrays/hard/longest_subarray_with_sum/solution.py
+++ ae_questions/arrays/hard/longest_subarray_with_sum/solution.py
@@ -2,7 +2,28 @@
 
 """
 def longest_subarray_with_sum(array, target):
-    return
+    longest_subarray = []
+    anchor, total_sum = -1, 0
+
+    for i in range(len(array)):
+        total_sum += array[i]
+        sum_at_anchor = 0 if anchor == -1 else array[anchor]
+        print('comp', total_sum - sum_at_anchor)
+        if total_sum - sum_at_anchor >= target:
+            # print("_________", i - anchor, anchor, longest_subarray)
+            if total_sum - sum_at_anchor == target and (
+                anchor == -1 or i - anchor > longest_subarray[1] - longest_subarray[0]
+            ):
+                longest_subarray = [anchor + 1, i]
+            anchor += 1
+        array[i] = total_sum
+        print(array)
+        print(anchor)
+        print(total_sum)
+        print(longest_subarray)
+        print("********")
+
+    return longest_subarray
 
 
 if __name__ == "__main__":
