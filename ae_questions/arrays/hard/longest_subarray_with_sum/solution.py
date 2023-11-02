6e02e53eefd2cdf6cf547ee4ff717f28f0943006
mattdepillis <mattdepillis@gmail.com>
2023-11-01 20:21:45 -0400

    longest subarray with sum - final solution

--- ae_questions/arrays/hard/longest_subarray_with_sum/solution.py
+++ ae_questions/arrays/hard/longest_subarray_with_sum/solution.py
@@ -8,23 +8,23 @@ def longest_subarray_with_sum(array, target):
     for i in range(len(array)):
         total_sum += array[i]
         sum_at_anchor = 0 if anchor == -1 else array[anchor]
-        print('comp', total_sum - sum_at_anchor)
-        if total_sum - sum_at_anchor == target:
-            if anchor == -1 or i - anchor > longest_subarray[1] - longest_subarray[0]:
+
+        if total_sum - sum_at_anchor == target or array[i] == target:
+            if array[i] == target or anchor == -1 or i - anchor > longest_subarray[1] - longest_subarray[0]:
                 longest_subarray = [anchor + 1, i]
         elif total_sum - sum_at_anchor > target: anchor += 1
         array[i] = total_sum
-        print(array)
-        print(anchor)
-        print(total_sum)
-        print(longest_subarray)
-        print("********")
 
     return longest_subarray
 
 
 if __name__ == "__main__":
+    # print(longest_subarray_with_sum(
+    #     [1, 2, 3, 4, 3, 3, 1, 2, 1],
+    #     10
+    # )) # [4, 8]
+
     print(longest_subarray_with_sum(
-        [1, 2, 3, 4, 3, 3, 1, 2, 1],
-        10
-    )) # [4, 8]
+        [1, 4, 10, 15, 31, 7, 1, 40, 0, 20, 1, 1, 1, 1, 2, 1],
+        0
+    ))
