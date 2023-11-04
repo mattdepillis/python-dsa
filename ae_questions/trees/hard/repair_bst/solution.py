182205eb3b03439a6191428f0e7bd29ce3f193de
mattdepillis <mattdepillis@gmail.com>
2023-11-04 11:51:34 -0400

    different starting test case + dfs algorithm to check on tree nodes

--- ae_questions/trees/hard/repair_bst/solution.py
+++ ae_questions/trees/hard/repair_bst/solution.py
@@ -7,21 +7,41 @@ class Node:
         self.left = None
         self.right = None
 
+def dfs(node, visited):
+    visited.append(node.value)
+
+    if node.left: dfs(node.left, visited)
+    if node.right: dfs(node.right, visited)
+
+    return visited
+
 def repair_bst(node):
     return node
 
 
 if __name__ == "__main__":
+    # nodes = [
+    #     {"id": "10", "left": "5", "right": "13", "value": 10},
+    #     {"id": "13", "left": "15", "right": "22", "value": 13},
+    #     {"id": "22", "left": None, "right": None, "value": 22},
+    #     {"id": "15", "left": None, "right": "14", "value": 15},
+    #     {"id": "14", "left": None, "right": None, "value": 14},
+    #     {"id": "5", "left": "2", "right": "6", "value": 5},
+    #     {"id": "6", "left": None, "right": None, "value": 6},
+    #     {"id": "2", "left": "1", "right": None, "value": 2},
+    #     {"id": "1", "left": None, "right": None, "value": 1}
+    # ]
+
     nodes = [
-        {"id": "10", "left": "5", "right": "13", "value": 10},
-        {"id": "13", "left": "15", "right": "22", "value": 13},
+        {"id": "10", "left": "7", "right": "20", "value": 10},
+        {"id": "7", "left": "3", "right": "12", "value": 7},
+        {"id": "12", "left": None, "right": None, "value": 12},
+        {"id": "20", "left": "8", "right": "22", "value": 20},
         {"id": "22", "left": None, "right": None, "value": 22},
-        {"id": "15", "left": None, "right": "14", "value": 15},
+        {"id": "3", "left": "2", "right": None, "value": 3},
+        {"id": "8", "left": None, "right": "14", "value": 8},
         {"id": "14", "left": None, "right": None, "value": 14},
-        {"id": "5", "left": "2", "right": "6", "value": 5},
-        {"id": "6", "left": None, "right": None, "value": 6},
-        {"id": "2", "left": "1", "right": None, "value": 2},
-        {"id": "1", "left": None, "right": None, "value": 1}
+        {"id": "2", "left": None, "right": None, "value": 2}
     ]
 
     store = {}
@@ -35,3 +55,5 @@ if __name__ == "__main__":
     root = store[nodes[0]["id"]]
     
     print(repair_bst(root))
+
+    print(dfs(root, []))
