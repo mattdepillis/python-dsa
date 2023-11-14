2b72c0abbcb1e3401b1a814e574477e5f6149ddb
mattdepillis <mattdepillis@gmail.com>
2023-11-13 22:47:03 -0500

    complete solution for search in range

--- ae_questions/searching/hard/search_for_range/solution.py
+++ ae_questions/searching/hard/search_for_range/solution.py
@@ -2,7 +2,6 @@
 
 """
 def check_subarray_middle(array, target, start, end, side):
-    print(side, array[start:end + 1])
     middle = ((end - start) // 2) + start
     shift = array[middle] < target if side == "left" else array[middle] > target
 
@@ -12,12 +11,8 @@ def check_subarray_middle(array, target, start, end, side):
 def search_for_range(array, target):
     start, end = 0, len(array) - 1
 
-    while start < end:
+    while start <= end:
         middle = ((end - start) // 2) + start
-
-        # print(f"start: {start}, end: {end}")
-
-        # print("middle", middle)
 
         shift_start, left_middle = check_subarray_middle(array, target, start, middle, "left")
         shift_end, right_middle = check_subarray_middle(array, target, middle + 1, end, "right")
@@ -33,4 +28,8 @@ def search_for_range(array, target):
 if __name__ == "__main__":
     print(search_for_range(
         [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73], target=45 # [4, 9]
+    ))
+
+    print(search_for_range(
+        [5, 7, 7, 8, 8, 10], 10
     ))
