72e88ec15194eafa7a1b5af86dc8668303f73739
mattdepillis <mattdepillis@gmail.com>
2023-11-19 18:05:34 -0500

    solution for linked_list_palindrome - not optimal SC

--- ae_questions/linked_lists/very_hard/linked_list_palindrome/solution.py
+++ ae_questions/linked_lists/very_hard/linked_list_palindrome/solution.py
@@ -6,8 +6,24 @@ class Node:
         self.value = value
         self.next = None
 
+def assign_prev_node(head):
+    prev, curr = None, head
+
+    while curr:
+        curr.prev = prev
+        curr, prev = curr.next, curr
+
+    return head, prev
+
 def linked_list_palindrome(head):
-    return
+    left, right = assign_prev_node(head)
+
+    while left is not right:
+        if left.value != right.value: return False
+        if left.next == right: break
+        left, right = left.next, right.prev
+
+    return True
 
 
 if __name__ == "__main__":
@@ -32,9 +48,9 @@ if __name__ == "__main__":
 
     head = store[head_id]
 
-    while head:
-        print(head.value)
-        head = head.next
+    # while head:
+    #     print(head.value)
+    #     head = head.next
 
 
     print(linked_list_palindrome(head))
